8f39f8417f29b604393b76cce77f3ede05b8d576
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-24 10:52:49 -0500

    Gateway with google login

---
+++ src/main/java/mx/com/devs4j/microservices/gateway/SimpleGatewayApplication.java
@@ -0,0 +1,13 @@
+package mx.com.devs4j.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGatewayApplication.class, args);
+	}
+	
+}
